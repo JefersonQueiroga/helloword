b75d3e8bfca6c26e73f76678c95e17fd616c41c0
jefersonqueiroga <jefersonqueiroga@gmail.com>
2022-06-05 17:28:37 -0300

    Adicionando alteracoes para teste

--- src/main/java/com/example/helloword/OAuth2Config.java
+++ src/main/java/com/example/helloword/OAuth2Config.java
@@ -37,7 +37,7 @@ public class OAuth2Config {
                 .clientId("1ac90dc9db1424af")
                 .clientSecret("0fdc47b869f38ae0128604c23d9e6363c8bb4fc1")
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("http://solarz.nadic.ifrn.edu.br/")
+                .redirectUri("http://solarz.nadic.ifrn.edu.br/teste3")
                 .authorizationUri("https://oauth.pipedrive.com/oauth/authorize")
                 .tokenUri("https://oauth.pipedrive.com/oauth/token")
                 .clientName("pipedrive")
