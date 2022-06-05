e51f2ad2ece918cff720f1299076c830934836bf
jefersonqueiroga <jefersonqueiroga@gmail.com>
2022-06-05 20:57:17 -0300

    Adicionar parâmetro para teste

--- src/main/java/com/example/helloword/TesteController.java
+++ src/main/java/com/example/helloword/TesteController.java
@@ -1,11 +1,25 @@
 package com.example.helloword;
 
 
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class TesteController {
+
+    @RequestMapping("/")
+    public Map<String, Object> home(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("display_name"));
+    }
+    @RequestMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("display_name"));
+    }
 
     @RequestMapping("/teste")
     public String teste(){
